1e998441baf3a8e3fd390cd96cfe5d11f44e5473
kyle <averytkyle@gmail.com>
2024-04-01 21:10:13 -0400

    Q9 done

--- problems/09-chain-map.js
+++ problems/09-chain-map.js
@@ -30,7 +30,14 @@ console.log(chainMap(4, half, square));         // 4
 *******************************************************************************/
 
 function chainMap(val, ...callbacks) {
-  // Your code here 
+  let result = val
+  if(callbacks === undefined) {
+    return result
+  } else {
+    callbacks.forEach(element => {
+      result = element(result)
+    })
+  } return result
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
